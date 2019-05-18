f30c4b755fa416fa1115adb19fa0133bed454cf5
Miguel Fernandes <miguelmanofernandes@gmail.com>
2019-05-18 02:28:04 +0100

    Fix dependencies, dev script and change desc on click

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-console */
 const vis = require('vis');
 
-
 const nodes = new vis.DataSet([
-  { id: 1, label: 'Node 1', title: 'mensagem x' },
-  { id: 2, label: 'Node 2' },
-  { id: 3, label: 'Node 3' },
-  { id: 4, label: 'Node 4' },
-  { id: 5, label: 'Node 5' },
+  { id: 1, label: 'Node 1', title: 'One' },
+  { id: 2, label: 'Node 2', title: 'Two' },
+  { id: 3, label: 'Node 3', title: 'Three' },
+  { id: 4, label: 'Node 4', title: 'Four' },
+  { id: 5, label: 'Node 5', title: 'Five' },
 ]);
 
 // create an array with edges
@@ -22,18 +21,11 @@ const edges = new vis.DataSet([
 const container = document.getElementById('mynetwork');
 
 // provide the data in the vis format
-const data = {
-  nodes,
-  edges,
-};
+const data = { nodes, edges };
 const options = {};
 
 // initialize your network!
 const network = new vis.Network(container, data, options);
 network.on('click', (properties) => {
-  const updated = nodes.get(properties[0]);
-  updated.title = 'cocó';
-  nodes.update(nodes.get(updated.id));
-  network.setData(data);
-  console.log(nodes);
+  nodes.update({ id: properties.nodes[0], title: 'I got clicked.' });
 });
