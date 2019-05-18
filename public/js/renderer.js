daf35b6051f01c54587b2170adf0bcba01bfaeaf
Miguel Fernandes <miguelmanofernandes@gmail.com>
2019-05-19 00:01:56 +0100

    Add custom node and edge

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -2,14 +2,31 @@
 const mdc = require('material-components-web');
 const graph = require('./graph.js');
 
+let node1;
+let node2;
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('button').forEach((btn) => { new mdc.ripple.MDCRipple(btn); });
 });
 
-document.querySelector('#add-node').addEventListener('click', () => {
-  graph.nodes.add({ label: 'New Node', title: 'ASDF' });
+
+const addNodeDialog = new mdc.dialog.MDCDialog(document.querySelector('#add-node-dialog'));
+
+addNodeDialog.listen('MDCDialog:closing', (evt) => {
+  if (evt.detail.action === 'yes') {
+    graph.nodes.add({ label: 'New Node', title: 'ASDF' });
+  }
 });
 
+document.querySelector('#add-node').addEventListener('click', () => addNodeDialog.open());
 document.querySelector('#add-edge').addEventListener('click', () => {
-  graph.edges.add({ from: 3, to: 4 });
+  if (node1 && node2) graph.edges.add({ from: node1, to: node2 });
+  node1 = undefined; node2 = undefined;
+});
+
+graph.network.on('click', (properties) => {
+  if (node2) { [node1] = properties.nodes; node2 = undefined; }
+
+  if (!node1) [node1] = properties.nodes;
+  else [node2] = properties.nodes;
 });
